bcf6840229005909d215e408e28a9aae7f487cdb
thebadsektor <gerald.villaran@lspu.edu.ph>
2021-02-03 14:24:09 +0800

    Clean and Build Success
    Clean and Build Success

---
+++ breadapp/src/test/java/com/SHVentureCapital/breadapp/BreadappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SHVentureCapital.breadapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
